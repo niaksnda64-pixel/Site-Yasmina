5d058fd391ccc5d63e98f5bea681b6de7852a6e8
niaksnda64-pixel <niaksnda64@gmail.com>
2026-04-27 14:46:51 +0200

    server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5500',
   'http://127.0.0.1:5500',
+  'https://mazavp.netlify.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
